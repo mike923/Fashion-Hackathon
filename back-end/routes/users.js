9465d7a2089db990a46abf9f03290d1d4ac1f6b3
vonbarown <vonielbrown@pursuit.org>
2020-02-23 11:17:01 -0500

    connecting the routes to the app.js file

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { getAllUsers, getUserByID } = require('../db/queries/users')
+const { requireLoginMid } = require('../auth/helpers')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', requireLoginMid, async (req, res, next) => {
+  try {
+    let users = await getAllUsers()
+    res.json({
+      payload: users,
+      msg: 'Retrieved all users',
+      err: false
+    })
+  } catch (error) {
+    res.status(500).json({
+      payload: null,
+      msg: 'Failed retrieving all users',
+      err: true
+    })
+  }
 });
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    let user = await getUserByID(req.params.id)
+    res.json({
+      payload: user,
+      msg: 'Retrieved all user',
+      err: false
+    })
+  } catch (error) {
+    console.log('error', error)
+    res.status(500).json({
+      payload: null,
+      msg: `Failed retrieving user by id:${req.params.id}`,
+      err: true
+    })
+  }
+  res.send(`you've hit GET /users/${req.params.id} route`)
+})
+
+router.post('/', (req, res, next) => {
+  res.send(`you've hit POST /users route`)
+})
 
 module.exports = router;
