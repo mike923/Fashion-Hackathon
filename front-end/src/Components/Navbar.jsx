ae83dea53c0a5e932dd26c9b78586d4e2e126f5d
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-02-23 17:47:24 -0500

    fixed navbar and change designercreateform

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -6,29 +6,25 @@ const Navbar = ({ logoutUser, isUserLoggedIn, user_id }) => (
         <h3>The Fanshion Something</h3>
         <ul className='nav-links'>
             <Link style={navStyle} to="/"><strong><li>Home</li></strong></Link>
-            <Link style={navStyle} to="/login"><strong><li>login</li></strong></Link>
-            <Link style={navStyle} to="/users/type"><strong><li>Designer</li></strong></Link>
-            <Link style={navStyle} to="/users/type/manufacturer/:id"><strong><li>Manufacturers</li></strong></Link>
-            <Link style={navStyle} to="/users/type/:id/order"><strong><li>Orders</li></strong></Link>
+            <Link style={navStyle} to="/users/type/designer"><strong><li>Designer</li></strong></Link>
             <Link style={navStyle} to="/users/type/:id/product"><strong><li>Products</li></strong></Link>
 
-            <Link to="/">Home</Link>{" "}
-            <Link to="/about">About</Link>{" "}
-            <Link to="/shows">Shows</Link>{" "}
-            {isUserLoggedIn 
-            ? (
-                <>
-                    <Link to={`/users/${user_id}/addShow`}>Add Show</Link>{" "}
-                    <Link to="/users">Users</Link>{" "}
-                    <Link to={`/users/${user_id}`}>Profile</Link>{" "}
-                    <button onClick={logoutUser}>Log-out</button>
-                </>
-            ) : (
-                <>
-                    <Link to="/login">Log-In</Link>{" "}
-                    <Link to="/signup">Sign-Up</Link>{" "}
-                </>
-            )}
+            {isUserLoggedIn
+                ? (
+                    <>
+                        <Link style={navStyle} to="/users/type/manufacturers"><strong><li>Manufacturers</li></strong></Link>
+                        <Link style={navStyle} to="/orders"><strong><li>Orders</li></strong></Link>
+                        <Link style={navStyle} to="/order"><strong><li>Orders</li></strong></Link>
+                        <Link style={navStyle} to="/users/type/manufacturer"><strong><li>Manufacturers</li></strong></Link>
+
+                        <button onClick={logoutUser}>Log-out</button>
+                    </>
+                ) : (
+                    <>
+                        <Link style={navStyle} to="/login"><strong><li>Log-In</li></strong></Link>
+                        <Link style={navStyle} to="/signup"><strong><li>Sign-Up</li></strong></Link>
+                    </>
+                )}
         </ul>
     </nav>
 )
