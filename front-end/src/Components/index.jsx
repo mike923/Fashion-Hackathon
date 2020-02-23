7f30fe09ea09d38c8c5c855c3540684056404bc3
MikePursuit <michaelamparo@pursuit.org>
2020-02-23 16:08:39 -0500

    added index

---
+++ front-end/src/Components/index.jsx
@@ -0,0 +1,25 @@
+import Nav from './Nav'
+import Home from './Home'
+import Login from './Login'
+import Orders from './Orders'
+import Navbar from './Navbar'
+import Signup from './Signup'
+import Products from './Products'
+import Designers from './Designers'
+import LoginForm from './LoginForm'
+import SignupForm from './SignupForm'
+import Manufacturers from './Manufacturers'
+
+export {
+    Nav,
+    Home,
+    Login,
+    Orders,
+    Navbar,
+    Signup,
+    Products,
+    Designers,
+    LoginForm,
+    SignupForm,
+    Manufacturers,
+}
