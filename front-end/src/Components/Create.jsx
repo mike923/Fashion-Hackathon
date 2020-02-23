1619711beb460f5c9c89a5f9d8ba26e8066e66c5
maliq <maliqtaylor@pursuit.org>
2020-02-23 16:06:34 -0500

    initialized component for creating a product

---
+++ front-end/src/Components/Create.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Create = () => {
+    const imageStyle = {
+        'border-style': 'solid',
+        'margin-right': '70%'
+    }
+  return (
+    <div>
+      <br />
+      <h1>Welcome to Creation</h1>
+      <img  style ={imageStyle} src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRa9NQl0OadsMFUDS-0ycSNaU7OJiBvgefKvC8m7SLkAph1V7ya" alt="default image"/>
+    </div>
+  );
+};
+
+export default Create;
