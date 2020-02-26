06334fe99c83a4a55c7bea91c30e4213a97b6287
vonbarown <vonielbrown@pursuit.org>
2020-02-26 17:13:18 -0500

    working on exporting tesseract file to be used later

--- back-end/tesseract/tesseract.js
+++ back-end/tesseract/tesseract.js
@@ -1,10 +1,22 @@
 const Tesseract = require('tesseract.js')
 
 
-Tesseract.recognize(
-    'https://tesseract.projectnaptha.com/img/eng_bw.png',
+const imgRec = (pic) =>{
+ try {
+  Tesseract.recognize(
+    pic,
     'eng',
     { logger: m => console.log(m) }
   ).then(({ data: { text } }) => {
     console.log(text);
   })
+ } catch (error) {
+   console.log(error);
+   
+ }
+}
+
+
+imgRec('https://tesseract.projectnaptha.com/img/eng_bw.png')
+
+module.exports = imgRec
