6bfafeca849fe2a4d6b92a90ac0a9a8da897c8f9
MikePursuit <michaelamparo@pursuit.org>
2020-02-25 20:08:47 -0500

    changes to backend

--- back-end/db/queries/manufactures.js
+++ back-end/db/queries/manufactures.js
@@ -1,19 +1,55 @@
 const db = require('../config')
 
-const getAllManufacturers = async () => await db.any('SELECT * FROM manufacturer')
+const getAllManufacturers = async () => await db.any(`
+    SELECT * 
+    FROM manufacturers
+`)
 
-const getDesignsByDesign = async (id) => await db.any(`
-    SELECT manufacturer_id, product_id,
-        JSON_OBJECT_AGG(
-            material, percentage_used
-        ) all_materials
-    FROM materials
-    JOIN materials_used
-    ON materials.id = materials_used.material_id
-    WHERE product_id = $1
-    GROUP BY manufacturer_id, product_id
+const getAllManufacturerEmployees = async () => await db.any(`
+	SELECT
+		users.id AS user_id, username, avatar_url, email, account_type, 
+		manufacture_id, manufacturer_name, specialty
+	FROM users
+	INNER JOIN manufacture_employee
+	ON employee_id = users.id
+	LEFT JOIN manufacturers
+	ON manufacturers.id = manufacture_id
+`)
+
+const getAllManufacturerEmployeesByCompany = async (id) => await db.any(`
+	SELECT
+		users.id AS user_id, username, avatar_url, email, account_type, 
+		manufacture_id, manufacturer_name, specialty
+	FROM users
+	INNER JOIN manufacture_employee
+	ON employee_id = users.id
+	LEFT JOIN manufacturers
+	ON manufacturers.id = manufacture_id
+	WHERE manufacture_id = $1
 `, [id])
 
+const getManufacturerEmployeeByID = async (id) => await db.one(`
+	SELECT
+		users.id AS user_id, username, avatar_url, email, account_type, 
+		manufacture_id, manufacturer_name, specialty
+	FROM users
+	INNER JOIN manufacture_employee
+	ON employee_id = users.id
+	LEFT JOIN manufacturers
+	ON manufacturers.id = manufacture_id
+	WHERE users.id = $1
+`, [id])
+
+const addNewManufacturer = async (employee_id, manufacture_id) => await db.one(`
+	INSERT INTO manufacture_employee (employee_id, manufacture_id)
+	VALUES ($1, $2/)
+	RETURNING *
+`, [employee_id, manufacture_id])
+
 module.exports = {
-    getMaterialsByDesign
+    addNewManufacturer,
+    getAllManufacturers,
+    getManufacturerEmployeeByID,
+    getAllManufacturerEmployees,
+    getAllManufacturerEmployeesByCompany,
 }
