e2daeeeb013cbf3e84102aca28fd8d41b94875fb
vonbarown <vonielbrown@pursuit.org>
2020-02-28 01:05:32 -0500

    all designers appear on the screen

--- front-end/src/Components/PublicDesigners.jsx
+++ front-end/src/Components/PublicDesigners.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
+const linkStyle = {
+    display: 'flex' ,
+    // alignItems:'center',
+    justifyContent:'center',
+    gap:'40px',
+    padding:'30px'
+}
 
-const PublicDesigners = (props) => {
+const PublicDesigners = ({ allDesigners }) => {
     return (
-        <div>
-            hello moto
+        <div className='public-designers'>
+            {
+                allDesigners.map(designer => {
+                    return (
+                        <Link to={`/designers/${designer.user_id}`} key={designer.user_id} style={linkStyle}>
+                            <div className='designer-profile-info' style={{
+                                display:'flex',
+                                gap:'10px'
+                            }}>
+                                <img src={designer.avatar_url}
+                                    alt={designer.username}
+                                    style={{
+                                        borderRadius: '70px',
+                                        height: '140px',
+                                        width: '140px'
+                                    }}
+                                />
+                                <p style={{fontSize:'30px'}}>{designer.username}</p>
+                            </div>
+                            <div className='designer-meta-data' style={{
+                                display:'flex',
+                                justifyContent:'flex-end',
+                                flexDirection:'column'
+                            }}>
+                                <p>Company: {designer.company_name}</p>
+                                <p>Email: {designer.email}</p>
+                            </div>
+                        </Link>
+                    )
+                })
+            }
         </div>
     )
 }
--- front-end/src/Containers/PublicContainer.jsx
+++ front-end/src/Containers/PublicContainer.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PublicDesigners from '../Components/PublicDesigners'
-
+import axios from 'axios'
+import { useEffect } from 'react'
 const PublicContainer = (props) =>{
-    return(
-        <div>
-        <PublicDesigners/>
-        </div>
-    )
+
+    const  [allDesigners, setAllDesigners] = useState([])
+
+    const getAllDesigners = async() =>{
+        try {
+            const {data: {payload}} = await axios.get(`/designers/all`)
+            console.log('all designers',payload);
+            setAllDesigners(payload)
+        } catch (error) {
+            
+        }
+    }
+
+    useEffect( () =>{
+        getAllDesigners()
+    },[])
+
+
+    return <PublicDesigners allDesigners ={allDesigners}/>
+ 
 }
 
 export default PublicContainer
