2671e9e8996d4095f4deebcca291b1682e6ea84f
vonbarown <vonielbrown@pursuit.org>
2020-02-28 00:39:02 -0500

    creating a public designers page

---
+++ front-end/src/Components/PublicDesigners.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const PublicDesigners = (props) => {
+    return (
+        <div>
+            hello moto
+        </div>
+    )
+}
+
+export default PublicDesigners
---
+++ front-end/src/Containers/PublicContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PublicDesigners from '../Components/PublicDesigners'
+
+const PublicContainer = (props) =>{
+    return(
+        <div>
+        <PublicDesigners/>
+        </div>
+    )
+}
+
+export default PublicContainer
