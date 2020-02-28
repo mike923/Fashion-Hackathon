26e86f70377339c60c90b8684293bc646ad0d2c8
MikePursuit <michaelamparo@pursuit.org>
2020-02-27 23:01:58 -0500

    fixed index.jsx error

--- front-end/src/Components/index.jsx
+++ front-end/src/Components/index.jsx
@@ -1,9 +1,6 @@
-import Nav from './Nav'
 import Home from './Home'
-import Login from './Login'
 import Orders from './Orders'
 import Navbar from './Navbar'
-import Signup from './Signup'
 import Products from './Products'
 import Designers from './OldDesigner/Designers'
 import LoginForm from './LoginForm'
@@ -11,12 +8,9 @@ import SignupForm from './SignupForm'
 import Manufacturers from './Manufacturers'
 
 export {
-    Nav,
     Home,
-    Login,
     Orders,
     Navbar,
-    Signup,
     Products,
     Designers,
     LoginForm,
